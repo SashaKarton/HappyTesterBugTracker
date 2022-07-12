596a9124b19ff03901335b8a03b78657ef060e5b
Sasha Karton <leshchenkoah@gmail.com>
2022-07-13 02:04:54 +0300

    Commented temporary Comment model.

--- HappyTesterWeb/Models/Comment.cs
+++ HappyTesterWeb/Models/Comment.cs
@@ -4,10 +4,10 @@ namespace HappyTesterWeb.Models
 {
     public class Comment
     {
-        [Key]
-        public int Id { get; set; }
-        public User Commenter { get; set; }
-        public string Message { get; set; }
-        public DateTime CommentDateTime { get; set; } = DateTime.Now;
+        //[Key]
+        //public int Id { get; set; }
+        //public User Commenter { get; set; }
+        //public string Message { get; set; }
+        //public DateTime CommentDateTime { get; set; } = DateTime.Now;
     }
 }
