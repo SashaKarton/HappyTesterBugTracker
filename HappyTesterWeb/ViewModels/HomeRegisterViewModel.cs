96f73131c15531e68e4694abbb5e7550de2a9b10
Sasha Karton <leshchenkoah@gmail.com>
2022-10-13 23:31:59 +0300

    Modified Home page

---
+++ HappyTesterWeb/ViewModels/HomeRegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HappyTesterWeb.ViewModels
+{
+    public class HomeRegisterViewModel
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "User name is required")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ HappyTesterWeb/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HappyTesterWeb.ViewModels
+{
+    public class HomeViewModel
+    {
+        public int NumberOfProjects { get; set; }
+        public int NumberOfNewTickets { get; set; }
+        public int NumberOfResolvedTickets { get; set; }        
+        public int NumberOfReopenedTickets { get; set; }
+
+    }
+}
