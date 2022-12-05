c0ac5c6a5811579029cebeb0d0357b6f26a610bc
Sasha Karton <leshchenkoah@gmail.com>
2022-12-05 02:02:09 +0200

    Added Unit Tests

---
+++ HappyTesterWeb.Tests/ControllerTests/ProjectControllerTests.cs
@@ -0,0 +1,63 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using HappyTesterWeb.Controllers;
+using HappyTesterWeb.Interfaces;
+using HappyTesterWeb.Models;
+using HappyTesterWeb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HappyTesterWeb.Tests.ControllerTests
+{
+    public class ProjectControllerTests
+    {
+        private ProjectController _projectController;
+        private IProjectRepository _projectRepository;
+        private ITicketRepository _ticketRepository;
+        
+        public ProjectControllerTests()
+        {
+            _projectRepository = A.Fake<IProjectRepository>();
+            _ticketRepository = A.Fake<ITicketRepository>();
+
+            //SUT
+            _projectController = new ProjectController(_projectRepository, _ticketRepository);
+        }
+
+        [Fact]
+        public void ProjectController_Index_ReturnsSuccess()
+        {
+            //Arrange - What do i need to bring in?
+            var projects = A.Fake<IEnumerable<Project>>();
+            A.CallTo(() => _projectRepository.GetAll()).Returns(projects);
+            var projectVMResult = A.Fake<List<IndexProjectViewModel>>();
+            foreach (var project in projects)
+            {
+                var projectVM = A.Fake<IndexProjectViewModel>();
+                projectVMResult.Add(projectVM);
+            }
+            //Act
+            var result = _projectController.Index();
+            //Assert - Object check actions
+            result.Should().BeOfType <Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void ProjectController_Detail_ReturnSuccess()
+        {
+            //Arrange
+            var projectId = 1;
+            var tickets = A.Fake<Project>();
+            A.CallTo(() => _projectRepository.GetTicketsByProjectIdAsync(projectId)).Returns(tickets);
+            //Act
+            var result = _projectController.Detail(projectId);
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+    }
+}
---
+++ HappyTesterWeb.Tests/RepositoryTests/TicketRepositoryTests.cs
@@ -0,0 +1,84 @@
+﻿using FluentAssertions;
+using HappyTesterWeb.Data;
+using HappyTesterWeb.Data.Enum;
+using HappyTesterWeb.Models;
+using HappyTesterWeb.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HappyTesterWeb.Tests.RepositoryTests
+{
+    public class TicketRepositoryTests
+    {
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new ApplicationDbContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (await databaseContext.Tickets.CountAsync() < 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    databaseContext.Tickets.Add(
+                        new Ticket()
+                        {
+                            Title = "Login Crash",
+                            Description = "When you run .exe file white screen pop and app closing enstantly after.",
+                            ProjectId = 1,
+                            IssuePriority = IssuePriorityEnum.High,
+                            TicketStatus = IssueStatusEnum.New,
+                            IssueType = IssueTypeEnum.Bug,
+                        });
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+            return databaseContext;
+        }
+
+        [Fact]
+        public async void TicketRepository_Add_ReturnsBool()
+        {
+            //Arrange
+            var ticket = new Ticket()
+            {
+                Title = "Login Crash",
+                Description = "When you run .exe file white screen pop and app closing enstantly after.",
+                ProjectId = 1,
+                IssuePriority = IssuePriorityEnum.High,
+                TicketStatus = IssueStatusEnum.New,
+                IssueType = IssueTypeEnum.Bug,
+            };
+            var dbContext = await GetDbContext();
+            var ticketRepository = new TicketRepository(dbContext);
+
+            //Act
+            var result = ticketRepository.Add(ticket);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async void TicketRepository_GetTicketByIdAsync_ReturnsTicket()
+        {
+            //Arrange
+            var id = 1;
+            var dbContext = await GetDbContext();
+            var ticketRepository = new TicketRepository(dbContext);
+
+            //Act
+            var result = ticketRepository.GetTicketByIdAsync(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Task<Ticket>>();
+        }
+    }
+}
